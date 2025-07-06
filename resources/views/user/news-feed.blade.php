338eede4cd6da9c19cc6cecf10cda5e603c24ae3
JahanzaibAns <jahanzebansari503@gmail.com>
2025-07-06 23:19:36 +0500

    alignment

--- resources/views/user/news-feed.blade.php
+++ resources/views/user/news-feed.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.main')
 @section('content')
 @section('styles')
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.13/cropper.min.css" rel="stylesheet" />
   <link rel="stylesheet" href="{{ asset('assets/css/components/news-feed.css') }}">
   <link rel="stylesheet" href="{{ asset('assets/css/components/post-card.css') }}">
   <link rel="stylesheet" href="{{ asset('assets/css/components/reactions.css') }}">
@@ -30,16 +31,14 @@
 
 @section('scripts')
     <script src="https://cdn.jsdelivr.net/npm/sortablejs@1.15.0/Sortable.min.js"></script>
-    <!-- CropperJS CSS & JS -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.13/cropper.min.css" rel="stylesheet" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.13/cropper.min.js"></script>
 
-  <script src="{{ asset('assets/js/components/modal.js') }}"></script>
-  <script src="{{ asset('assets/js/components/post-expand.js') }}"></script>
-  <script src="{{ asset('assets/js/components/reactions.js') }}"></script>
-  <script src="{{ asset('assets/js/components/emoji-picker.js') }}" type="module"></script>
-  <script src="{{ asset('assets/js/components/image-upload.js') }}"></script>
-  <script src="{{ asset('assets/js/components/cropper-editor.js') }}"></script>
-  <script src="{{ asset('assets/js/components/toggle.js') }}"></script>
+    <script src="{{ asset('assets/js/components/modal.js') }}"></script>
+    <script src="{{ asset('assets/js/components/post-expand.js') }}"></script>
+    <script src="{{ asset('assets/js/components/reactions.js') }}"></script>
+    <script src="{{ asset('assets/js/components/emoji-picker.js') }}" type="module"></script>
+    <script src="{{ asset('assets/js/components/image-upload.js') }}"></script>
+    <script src="{{ asset('assets/js/components/cropper-editor.js') }}"></script>
+    <script src="{{ asset('assets/js/components/toggle.js') }}"></script>
 
 @endsection
